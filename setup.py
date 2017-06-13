cbfd5f23bfb25667e2f252c51a88790e33a4274e
Chapman Siu <chpmn.siu@gmail.com>
2017-06-13 19:23:39 +1000

    updating for pypi upload

--- setup.py
+++ setup.py
@@ -1,8 +1,24 @@
 from distutils.core import setup
 
-NAME = "skfeature"
+"""
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
 
-DESCRIPTION = "Feature Selection Repository in Python (DMML Lab@ASU)"
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+
+NAME = "skfeature-chappers"
+
+DESCRIPTION = "Unofficial Fork of Feature Selection Repository in Python (DMML Lab@ASU)"
 
 KEYWORDS = "Feature Selection Repository"
 
@@ -10,9 +26,13 @@ AUTHOR = "Jundong Li, Kewei Cheng, Suhang Wang"
 
 AUTHOR_EMAIL = "jundong.li@asu.edu, kcheng18@asu.edu, suhang.wang@asu.edu"
 
-URL = "https://github.com/jundongl/scikit-feature"
+MAINTAINER="Chapman Siu"
 
-VERSION = "1.0.0"
+MAINTAINER_EMAIL="chpmn.siu@gmail.com"
+
+URL = "https://github.com/chappers/scikit-feature"
+
+VERSION = "1.0.1"
 
 
 setup(
@@ -22,6 +42,8 @@ setup(
     keywords = KEYWORDS,
     author = AUTHOR,
     author_email = AUTHOR_EMAIL,
+    maintainer=MAINTAINER, 
+    maintainer_email=MAINTAINER_EMAIL,
     url = URL,
     packages =['skfeature', 'skfeature.utility','skfeature.function','skfeature.function.information_theoretical_based','skfeature.function.similarity_based','skfeature.function.sparse_learning_based','skfeature.function.statistical_based','skfeature.function.streaming','skfeature.function.structure','skfeature.function.wrapper',] ,
 )
